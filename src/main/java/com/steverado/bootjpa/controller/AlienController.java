816f71beb326e22d7c469ed87783ede41f585320
stephen <isaacsteverado9@gmail.com>
2025-12-15 02:12:42 +0100

    getting data from the url

--- src/main/java/com/steverado/bootjpa/controller/AlienController.java
+++ src/main/java/com/steverado/bootjpa/controller/AlienController.java
@@ -5,8 +5,10 @@ import com.steverado.bootjpa.model.Alien;
 import org.apache.el.parser.AstGreaterThan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -28,16 +30,30 @@ public class AlienController {
         return "home";
     }
 
-    @RequestMapping("/getAlien")
-    public ModelAndView getALien(@RequestParam int aid) {
-        ModelAndView mv = new ModelAndView("showAlien");
-        Alien alien = repo.findById(aid).orElse(new Alien());
-
-        System.out.println(repo.findByTech("Java"));
-        System.out.println(repo.findByAidGreaterThan(102));
-        System.out.println(repo.findByTechSorted("Java"));
-
-        mv.addObject(alien);
-        return mv;
+    @RequestMapping("/aliens")
+    @ResponseBody
+    public String getALiens() {
+        //converting the returned iterable to string
+        return repo.findAll().toString() ;
     }
+
+    @RequestMapping("/alien/{aid}")
+    @ResponseBody
+    public String getALien(@PathVariable("aid") int aid) {
+        //converting the returned iterable to string
+        return repo.findById(aid).toString() ;
+    }
+
+//    @RequestMapping("/getAlien")
+//    public ModelAndView getALien(@RequestParam int aid) {
+//        ModelAndView mv = new ModelAndView("showAlien");
+//        Alien alien = repo.findById(aid).orElse(new Alien());
+//
+//        System.out.println(repo.findByTech("Java"));
+//        System.out.println(repo.findByAidGreaterThan(102));
+//        System.out.println(repo.findByTechSorted("java"));
+//
+//        mv.addObject(alien);
+//        return mv;
+//    }
 }
--- src/main/java/com/steverado/bootjpa/dao/AlienRepo.java
+++ src/main/java/com/steverado/bootjpa/dao/AlienRepo.java
@@ -7,10 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AlienRepo extends CrudRepository<Alien, Integer> {
-    List<Alien> findByTech(String tech);
 
-    List<Alien> findByAidGreaterThan(int aid);
-
-    @Query("from Alien where tech=?1 order by aname ")
-    List<Alien> findByTechSorted(String tech);
 }
